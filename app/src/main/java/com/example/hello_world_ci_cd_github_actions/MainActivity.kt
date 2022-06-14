969b19317d9ac649f266d0088f84a00e9ffd2686
karimelbahi <k.a.ebahi@gmail.com>
2022-06-14 15:16:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hello_world_ci_cd_github_actions/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hello_world_ci_cd_github_actions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
